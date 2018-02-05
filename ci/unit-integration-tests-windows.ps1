cf9377ce6184a36e89da99fa023856c88e4ac81a
Tom Godkin <tgodkin@pivotal.io>
2018-02-05 11:50:03 +0000

    Vendor dependencies with dep
    [#154874565]

--- ci/unit-integration-tests-windows.ps1
+++ ci/unit-integration-tests-windows.ps1
@@ -6,18 +6,9 @@ $env:PATH = $env:GOPATH + "/bin;" + $env:PATH
 
 go version
 
-push-location "src\code.cloudfoundry.org\groot"
-  # We don't pass -u, so this won't fetch a later revision of groot than the one
-  # we are supposed to be testing.
-  go get -v -t
-  if ($LastExitCode -ne 0) {
-    exit $LastExitCode
-  }
-pop-location
-
 go get github.com/golang/protobuf
 Write-Host "Installing Ginkgo"
-go install ./src/github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/ginkgo/ginkgo
 if ($LastExitCode -ne 0) {
     throw "Ginkgo installation process returned error code: $LastExitCode"
 }
