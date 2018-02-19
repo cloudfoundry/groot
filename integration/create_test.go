4b8055767e1954a4af876e9d2384444bf6a2b003
Georgi Sabev <georgethebeatle@gmail.com>
2018-02-19 11:08:51 +0200

    Remove duplicate tests
    No need to test containers image via groot integration tests
    
    [finishes #155149117]

--- integration/create_test.go
+++ integration/create_test.go
@@ -203,139 +203,6 @@ var _ = Describe("create", func() {
 	})
 
 	Describe("Remote images", func() {
-		BeforeEach(func() {
-			imageSize = 297
-			rootfsURI = "docker:///cfgarden/three-layers"
-		})
-
-		It("does not return an error", func() {
-			Expect(footExitErr).NotTo(HaveOccurred())
-		})
-
-		It("calls driver.Unpack() with the expected args", func() {
-			var args foot.UnpackCalls
-			unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &args)
-			Expect(args[0].ID).NotTo(BeEmpty())
-			Expect(args[0].ParentIDs).To(BeEmpty())
-		})
-
-		Describe("subsequent invocations", func() {
-			It("generates the same layer ID", func() {
-				var unpackArgs foot.UnpackCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-				firstInvocationLayerID := unpackArgs[0].ID
-
-				_, err := runFoot(configFilePath, driverStoreDir, "create", rootfsURI, "some-handle")
-				Expect(err).NotTo(HaveOccurred())
-
-				unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-				secondInvocationLayerID := unpackArgs[0].ID
-
-				Expect(secondInvocationLayerID).To(Equal(firstInvocationLayerID))
-			})
-		})
-
-		Describe("layer caching", func() {
-			It("calls exists", func() {
-				var existsArgs foot.ExistsCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.ExistsArgsFileName), &existsArgs)
-				Expect(existsArgs[0].LayerID).ToNot(BeEmpty())
-			})
-
-			It("calls driver.Unpack() with the layerID", func() {
-				var existsArgs foot.ExistsCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.ExistsArgsFileName), &existsArgs)
-				Expect(existsArgs[0].LayerID).ToNot(BeEmpty())
-
-				Expect(filepath.Join(driverStoreDir, foot.UnpackArgsFileName)).To(BeAnExistingFile())
-
-				var unpackArgs foot.UnpackCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-				Expect(len(unpackArgs)).To(Equal(len(existsArgs)))
-
-				lastCall := len(unpackArgs) - 1
-				for i := range unpackArgs {
-					Expect(unpackArgs[i].ID).To(Equal(existsArgs[lastCall-i].LayerID))
-				}
-			})
-
-			Context("when the layer is cached", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_LAYER_EXISTS=true")
-				})
-
-				It("doesn't call driver.Unpack()", func() {
-					Expect(filepath.Join(driverStoreDir, foot.UnpackArgsFileName)).ToNot(BeAnExistingFile())
-				})
-			})
-		})
-
-		It("calls driver.Bundle() with expected args", func() {
-			var unpackArgs foot.UnpackCalls
-			unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-
-			var bundleArgs foot.BundleCalls
-			unmarshalFile(filepath.Join(driverStoreDir, foot.BundleArgsFileName), &bundleArgs)
-			unpackLayerIds := []string{}
-			for _, call := range unpackArgs {
-				unpackLayerIds = append(unpackLayerIds, call.ID)
-			}
-			Expect(bundleArgs[0].ID).To(Equal("some-handle"))
-			Expect(bundleArgs[0].LayerIDs).To(ConsistOf(unpackLayerIds))
-			Expect(bundleArgs[0].DiskLimit).To(Equal(expectedDiskLimit))
-		})
-
-		It("calls driver.WriteMetadata() with expected args", func() {
-			var writeMetadataArgs foot.WriteMetadataCalls
-			unmarshalFile(filepath.Join(driverStoreDir, foot.WriteMetadataArgsFileName), &writeMetadataArgs)
-
-			Expect(writeMetadataArgs[0].ID).To(Equal("some-handle"))
-			Expect(writeMetadataArgs[0].VolumeData).To(Equal(groot.VolumeMetadata{BaseImageSize: imageSize}))
-		})
-
-		Context("--disk-limit-size-bytes is given", func() {
-			BeforeEach(func() {
-				createOptions = []string{"--disk-limit-size-bytes", "500"}
-			})
-
-			It("calls driver.Bundle() with expected args", func() {
-				var unpackArgs foot.UnpackCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-
-				var bundleArgs foot.BundleCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.BundleArgsFileName), &bundleArgs)
-				unpackLayerIds := []string{}
-				for _, call := range unpackArgs {
-					unpackLayerIds = append(unpackLayerIds, call.ID)
-				}
-				Expect(bundleArgs[0].ID).To(Equal("some-handle"))
-				Expect(bundleArgs[0].LayerIDs).To(ConsistOf(unpackLayerIds))
-				Expect(bundleArgs[0].DiskLimit).To(Equal(500 - imageSize))
-			})
-
-			Context("--exclude-image-from-quota is given as well", func() {
-				BeforeEach(func() {
-					createOptions = []string{"--disk-limit-size-bytes", "500", "--exclude-image-from-quota"}
-				})
-
-				It("calls driver.Bundle() with expected args", func() {
-					var unpackArgs foot.UnpackCalls
-					unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-
-					var bundleArgs foot.BundleCalls
-					unmarshalFile(filepath.Join(driverStoreDir, foot.BundleArgsFileName), &bundleArgs)
-					unpackLayerIds := []string{}
-					for _, call := range unpackArgs {
-						unpackLayerIds = append(unpackLayerIds, call.ID)
-					}
-					Expect(bundleArgs[0].ID).To(Equal("some-handle"))
-					Expect(bundleArgs[0].LayerIDs).To(ConsistOf(unpackLayerIds))
-					Expect(bundleArgs[0].DiskLimit).To(Equal(int64(500)))
-				})
-			})
-		})
-
-		// TODO: especially for remote images
 		Context("when the image has multiple layers", func() {
 			It("correctly passes parent IDs to each driver.Unpack() call", func() {
 				var args foot.UnpackCalls
@@ -381,143 +248,73 @@ var _ = Describe("create", func() {
 			})
 		})
 
-		Describe("Local Images", func() {
-			Context("when driver.Unpack() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_UNPACK_ERROR=true")
-				})
-
-				It("prints the error", func() {
-					Expect(footStdout).To(ContainSubstring("unpack-err"))
-				})
+		Context("when driver.Unpack() returns an error", func() {
+			BeforeEach(func() {
+				env = append(env, "FOOT_UNPACK_ERROR=true")
 			})
 
-			Context("when the config file path is not an existing file", func() {
-				BeforeEach(func() {
-					Expect(os.Remove(configFilePath)).To(Succeed())
-				})
-
-				It("prints an error", func() {
-					Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
-				})
+			It("prints the error", func() {
+				Expect(footStdout).To(ContainSubstring("unpack-err"))
 			})
-
-			Context("when the config file is invalid yaml", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "%haha")
-				})
-
-				It("prints an error", func() {
-					Expect(footStdout).To(ContainSubstring("yaml"))
-				})
-			})
-
-			Context("when the specified log level is invalid", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "log_level: lol")
-				})
-
-				It("prints an error", func() {
-					Expect(footStdout).To(ContainSubstring("lol"))
-				})
-			})
-
-			Context("when driver.Bundle() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_BUNDLE_ERROR=true")
-				})
-
-				It("prints the error", func() {
-					Expect(footStdout).To(ContainSubstring("bundle-err"))
-				})
-			})
-
-			Context("when driver.WriteMetadata() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_WRITE_METADATA_ERROR=true")
-				})
-
-				It("prints the error", func() {
-					Expect(footStdout).To(ContainSubstring("write-metadata-err"))
-				})
-			})
-
-			//TODO: especially for local images
-			Context("when the rootfs URI is not a file", func() {
-				BeforeEach(func() {
-					Expect(os.Remove(rootfsURI)).To(Succeed())
-				})
-
-				It("prints an error", func() {
-					Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
-				})
-			})
-
 		})
 
-		Describe("Remote Images", func() {
+		Context("when the config file path is not an existing file", func() {
 			BeforeEach(func() {
-				rootfsURI = "docker:///cfgarden/three-layers"
+				Expect(os.Remove(configFilePath)).To(Succeed())
 			})
 
-			Context("when driver.Unpack() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_UNPACK_ERROR=true")
-				})
+			It("prints an error", func() {
+				Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
+			})
+		})
 
-				It("prints the error", func() {
-					Expect(footStdout).To(ContainSubstring("unpack-err"))
-				})
+		Context("when the config file is invalid yaml", func() {
+			BeforeEach(func() {
+				writeFile(configFilePath, "%haha")
 			})
 
-			Context("when the config file path is not an existing file", func() {
-				BeforeEach(func() {
-					Expect(os.Remove(configFilePath)).To(Succeed())
-				})
+			It("prints an error", func() {
+				Expect(footStdout).To(ContainSubstring("yaml"))
+			})
+		})
 
-				It("prints an error", func() {
-					Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
-				})
+		Context("when the specified log level is invalid", func() {
+			BeforeEach(func() {
+				writeFile(configFilePath, "log_level: lol")
 			})
 
-			Context("when the config file is invalid yaml", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "%haha")
-				})
+			It("prints an error", func() {
+				Expect(footStdout).To(ContainSubstring("lol"))
+			})
+		})
 
-				It("prints an error", func() {
-					Expect(footStdout).To(ContainSubstring("yaml"))
-				})
+		Context("when driver.Bundle() returns an error", func() {
+			BeforeEach(func() {
+				env = append(env, "FOOT_BUNDLE_ERROR=true")
 			})
 
-			Context("when the specified log level is invalid", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "log_level: lol")
-				})
+			It("prints the error", func() {
+				Expect(footStdout).To(ContainSubstring("bundle-err"))
+			})
+		})
 
-				It("prints an error", func() {
-					Expect(footStdout).To(ContainSubstring("lol"))
-				})
+		Context("when driver.WriteMetadata() returns an error", func() {
+			BeforeEach(func() {
+				env = append(env, "FOOT_WRITE_METADATA_ERROR=true")
 			})
 
-			Context("when driver.Bundle() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_BUNDLE_ERROR=true")
-				})
+			It("prints the error", func() {
+				Expect(footStdout).To(ContainSubstring("write-metadata-err"))
+			})
+		})
 
-				It("prints the error", func() {
-					Expect(footStdout).To(ContainSubstring("bundle-err"))
-				})
+		Context("when the rootfs URI is not a file", func() {
+			BeforeEach(func() {
+				Expect(os.Remove(rootfsURI)).To(Succeed())
 			})
 
-			Context("when driver.WriteMetadata() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_WRITE_METADATA_ERROR=true")
-				})
-
-				It("prints the error", func() {
-					Expect(footStdout).To(ContainSubstring("write-metadata-err"))
-				})
+			It("prints an error", func() {
+				Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
 			})
 		})
 	})
--- integration/pull_test.go
+++ integration/pull_test.go
@@ -133,68 +133,6 @@ var _ = Describe("pull", func() {
 			rootfsURI = "docker:///cfgarden/three-layers"
 		})
 
-		It("does not return an error", func() {
-			Expect(footExitErr).NotTo(HaveOccurred())
-		})
-
-		It("calls driver.Unpack() with the expected args", func() {
-			var args foot.UnpackCalls
-			unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &args)
-			Expect(args[0].ID).NotTo(BeEmpty())
-			Expect(args[0].ParentIDs).To(BeEmpty())
-		})
-
-		Describe("subsequent invocations", func() {
-			It("generates the same layer ID", func() {
-				var unpackArgs foot.UnpackCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-				firstInvocationLayerID := unpackArgs[0].ID
-
-				_, err := runFoot(configFilePath, driverStoreDir, "pull", rootfsURI)
-				Expect(err).NotTo(HaveOccurred())
-
-				unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-				secondInvocationLayerID := unpackArgs[0].ID
-
-				Expect(secondInvocationLayerID).To(Equal(firstInvocationLayerID))
-			})
-		})
-
-		Describe("layer caching", func() {
-			It("calls exists", func() {
-				var existsArgs foot.ExistsCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.ExistsArgsFileName), &existsArgs)
-				Expect(existsArgs[0].LayerID).ToNot(BeEmpty())
-			})
-
-			It("calls driver.Unpack() with the layerID", func() {
-				var existsArgs foot.ExistsCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.ExistsArgsFileName), &existsArgs)
-				Expect(existsArgs[0].LayerID).ToNot(BeEmpty())
-
-				Expect(filepath.Join(driverStoreDir, foot.UnpackArgsFileName)).To(BeAnExistingFile())
-
-				var unpackArgs foot.UnpackCalls
-				unmarshalFile(filepath.Join(driverStoreDir, foot.UnpackArgsFileName), &unpackArgs)
-				Expect(len(unpackArgs)).To(Equal(len(existsArgs)))
-
-				lastCall := len(unpackArgs) - 1
-				for i := range unpackArgs {
-					Expect(unpackArgs[i].ID).To(Equal(existsArgs[lastCall-i].LayerID))
-				}
-			})
-
-			Context("when the layer is cached", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_LAYER_EXISTS=true")
-				})
-
-				It("doesn't call driver.Unpack()", func() {
-					Expect(filepath.Join(driverStoreDir, foot.UnpackArgsFileName)).ToNot(BeAnExistingFile())
-				})
-			})
-		})
-
 		Context("when the image has multiple layers", func() {
 			It("correctly passes parent IDs to each driver.Unpack() call", func() {
 				var args foot.UnpackCalls
@@ -210,110 +148,58 @@ var _ = Describe("pull", func() {
 	})
 
 	Describe("failure", func() {
-		Describe("Local Images", func() {
-			Context("when driver.Unpack() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_UNPACK_ERROR=true")
-				})
-
-				It("prints the error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring("unpack-err"))
-				})
+		Context("when driver.Unpack() returns an error", func() {
+			BeforeEach(func() {
+				env = append(env, "FOOT_UNPACK_ERROR=true")
 			})
 
-			Context("when the config file path is not an existing file", func() {
-				BeforeEach(func() {
-					Expect(os.Remove(configFilePath)).To(Succeed())
-				})
-
-				It("prints an error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
-				})
-			})
-
-			Context("when the config file is invalid yaml", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "%haha")
-				})
-
-				It("prints an error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring("yaml"))
-				})
-			})
-
-			Context("when the specified log level is invalid", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "log_level: lol")
-				})
-
-				It("prints an error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring("lol"))
-				})
-			})
-
-			Context("when the rootfs URI is not a file", func() {
-				BeforeEach(func() {
-					Expect(os.Remove(rootfsURI)).To(Succeed())
-				})
-
-				It("prints an error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
-				})
+			It("prints the error", func() {
+				Expect(footExitErr).To(HaveOccurred())
+				Expect(footStdout).To(ContainSubstring("unpack-err"))
 			})
 		})
 
-		Describe("Remote Images", func() {
+		Context("when the config file path is not an existing file", func() {
 			BeforeEach(func() {
-				rootfsURI = "docker:///cfgarden/three-layers"
+				Expect(os.Remove(configFilePath)).To(Succeed())
 			})
 
-			Context("when driver.Unpack() returns an error", func() {
-				BeforeEach(func() {
-					env = append(env, "FOOT_UNPACK_ERROR=true")
-				})
+			It("prints an error", func() {
+				Expect(footExitErr).To(HaveOccurred())
+				Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
+			})
+		})
 
-				It("prints the error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring("unpack-err"))
-				})
+		Context("when the config file is invalid yaml", func() {
+			BeforeEach(func() {
+				writeFile(configFilePath, "%haha")
 			})
 
-			Context("when the config file path is not an existing file", func() {
-				BeforeEach(func() {
-					Expect(os.Remove(configFilePath)).To(Succeed())
-				})
+			It("prints an error", func() {
+				Expect(footExitErr).To(HaveOccurred())
+				Expect(footStdout).To(ContainSubstring("yaml"))
+			})
+		})
 
-				It("prints an error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
-				})
+		Context("when the specified log level is invalid", func() {
+			BeforeEach(func() {
+				writeFile(configFilePath, "log_level: lol")
 			})
 
-			Context("when the config file is invalid yaml", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "%haha")
-				})
+			It("prints an error", func() {
+				Expect(footExitErr).To(HaveOccurred())
+				Expect(footStdout).To(ContainSubstring("lol"))
+			})
+		})
 
-				It("prints an error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring("yaml"))
-				})
+		Context("when the rootfs URI is not a file", func() {
+			BeforeEach(func() {
+				Expect(os.Remove(rootfsURI)).To(Succeed())
 			})
 
-			Context("when the specified log level is invalid", func() {
-				BeforeEach(func() {
-					writeFile(configFilePath, "log_level: lol")
-				})
-
-				It("prints an error", func() {
-					Expect(footExitErr).To(HaveOccurred())
-					Expect(footStdout).To(ContainSubstring("lol"))
-				})
+			It("prints an error", func() {
+				Expect(footExitErr).To(HaveOccurred())
+				Expect(footStdout).To(ContainSubstring(notFoundRuntimeError[runtime.GOOS]))
 			})
 		})
 	})
