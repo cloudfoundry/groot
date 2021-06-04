c69e9c5dd44996bd93785a949a4da9517c74a888
Kieron Browne <kbrowne@vmware.com>
2021-06-04 15:03:37 +0100

    Make a url which is invalid on windows with go 1.16
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- fetcher/layerfetcher/source/layer_source_oci_test.go
+++ fetcher/layerfetcher/source/layer_source_oci_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Layer source: OCI", func() {
 
 		Context("when the image url is invalid", func() {
 			BeforeEach(func() {
-				imageURL = urlParse("oci://///cfgarden/empty:v0.1.0")
+				imageURL = urlParse("oci://///\\cfgarden/empty:v0.1.0")
 			})
 			It("returns an error", func() {
 				_, err := layerSource.Manifest(logger)
